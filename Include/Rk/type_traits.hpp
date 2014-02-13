cd13cd20cc21d1780f318f5bae929c98333527ff
Ben Peggs <b.peggs@googlemail.com>
2014-02-13 00:04:51 +0000

    Added some type traits.

--- Include/Rk/type_traits.hpp
+++ Include/Rk/type_traits.hpp
@@ -28,4 +28,59 @@ namespace Rk
 
   };
   
+  template <typename lhs_t, typename rhs_t>
+  using sum_t        = decltype (std::declval <lhs_t> () + std::declval <rhs_t> ());
+
+  template <typename lhs_t, typename rhs_t>
+  using difference_t = decltype (std::declval <lhs_t> () - std::declval <rhs_t> ());
+
+  template <typename lhs_t, typename rhs_t>
+  using product_t    = decltype (std::declval <lhs_t> () * std::declval <rhs_t> ());
+
+  template <typename lhs_t, typename rhs_t>
+  using quotient_t   = decltype (std::declval <lhs_t> () / std::declval <rhs_t> ());
+
+  template <typename lhs_t, typename rhs_t>
+  using remainder_t  = decltype (std::declval <lhs_t> () % std::declval <rhs_t> ());
+
+  template <typename lhs_t, typename rhs_t>
+  struct /*using*/ const_and_t /*=*/ : std::integral_constant <bool, lhs_t::value && rhs_t::value> { } ;
+
+  namespace traits_private
+  {
+    template <typename... ts>
+    struct is_same_comp;
+
+    template <typename t>
+    struct is_same_comp <t, t> :
+      std::true_type
+    { };
+    
+    template <typename t, typename u>
+    struct is_same_comp <t, u> :
+      std::false_type
+    { };
+    
+    template <typename t, typename u, typename... ts>
+    struct is_same_comp <t, u, ts...> :
+      const_and_t <is_same_comp <t, u>, is_same_comp <u, ts...>>
+    { };
+    
+    template <typename t, bool same>
+    struct is_same_impl :
+      std::false_type
+    { };
+
+    template <typename t>
+    struct is_same_impl <t, true> :
+      std::true_type
+    {
+      typedef t same_t;
+    };
+
+  }
+
+  template <typename t, typename... ts>
+  using is_same = traits_private::is_same_impl <t, traits_private::is_same_comp <t, ts...>::value>;
+
 }
